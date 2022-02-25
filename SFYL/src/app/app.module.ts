5fe264568a678e5d70dec2cb6626bfb30313af76
tranvanhao016a <hao.tv1665@sinhvien.hoasen.edu.vn>
2022-02-25 10:14:44 +0700

    Hao-Chat

--- SFYL/src/app/app.module.ts
+++ SFYL/src/app/app.module.ts
@@ -10,11 +10,13 @@ import { environment } from '../environments/environment';
 import { provideAuth,getAuth } from '@angular/fire/auth';
 import { provideFirestore,getFirestore } from '@angular/fire/firestore';
 
+
 @NgModule({
   declarations: [
     AppComponent,
     NavBarComponent,
-    SideBarComponent
+    SideBarComponent,
+
   ],
   imports: [
     BrowserModule,
