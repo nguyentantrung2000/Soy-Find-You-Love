c36dbbbb9eb8308a1d322a462ed747818c678bd5
tranvanhao016a <hao.tv1665@sinhvien.hoasen.edu.vn>
2022-02-25 16:18:41 +0700

    Hao pull

--- SFYL/src/app/share/share/share.module.ts
+++ SFYL/src/app/share/share/share.module.ts
@@ -1,14 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ReactiveFormsModule } from '@angular/forms';
-
+import {MatIconModule} from '@angular/material/icon';
 @NgModule({
   declarations: [
     
   ],
   imports: [CommonModule,
-           ReactiveFormsModule
+           ReactiveFormsModule,
+           MatIconModule
             ],
-  exports: [ReactiveFormsModule],
+  exports: [ReactiveFormsModule,MatIconModule],
 })
 export class ShareModule {}
