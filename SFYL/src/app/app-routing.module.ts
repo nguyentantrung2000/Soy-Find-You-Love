4229d78f8784d6ce9a8a3f4756d2b00fc525bf61
Bobb <phong.vt5575@sinhvien.hoasen.edu.vn>
2022-02-25 16:08:15 +0700

    Phong lam match xong roi nhá

--- SFYL/src/app/app-routing.module.ts
+++ SFYL/src/app/app-routing.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [{ path: 'homePage', loadChildren: () => import('./pages/home-page/home-page.module').then(m => m.HomePageModule) }];
+const routes: Routes = [{ path: 'homePage', loadChildren: () => import('./pages/home-page/home-page.module').then(m => m.HomePageModule) }, { path: 'match', loadChildren: () => import('./pages/match/match.module').then(m => m.MatchModule) }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
