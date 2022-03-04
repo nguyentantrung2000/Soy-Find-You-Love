98e1d65a9c9cd8783f27ef0fcde2ba9113ace2c3
nguyentantrung2000 <trung.nt4996@sinhvien.hoasen.edu.vn>
2022-03-04 14:08:38 +0700

    pull

--- Server/index.js
+++ Server/index.js
@@ -1,6 +1,15 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const server = express();
+
+var admin = require("firebase-admin");
+
+var serviceAccount = require("./firebase-admin.json");
+
+admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount)
+});
+
 server.use(bodyParser.json());
 
 server.listen(3000, () => {
