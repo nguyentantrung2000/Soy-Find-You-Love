c440b9d8dabbd547326e10506a13e87cd8ce06da
Bui Thanh Huy <huy.bt8686@sinhvien.hoasen.edu.vn>
2022-03-10 09:52:32 +0700

    nop bai

--- SFYL/src/app/share/share/share.module.ts
+++ SFYL/src/app/share/share/share.module.ts
@@ -12,6 +12,7 @@ import { FormsModule } from '@angular/forms';
 import { DialogRegisterComponent } from '../../components/dialog-register/dialog-register.component';
 import { CheckEmailComponent } from 'src/app/components/check-email/check-email.component';
 import { SigninSuccessComponent } from 'src/app/components/signin-success/signin-success.component';
+import { RecoverAccountComponent } from 'src/app/components/recover-account/recover-account.component';
 @NgModule({
   declarations: [
     NavBarComponent,
@@ -20,6 +21,7 @@ import { SigninSuccessComponent } from 'src/app/components/signin-success/signin
     DialogRegisterComponent,
     CheckEmailComponent,
     SigninSuccessComponent,
+    RecoverAccountComponent,
   ],
   imports: [
     CommonModule,
