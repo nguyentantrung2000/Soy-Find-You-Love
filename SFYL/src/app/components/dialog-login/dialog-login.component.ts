0533afb09ee5b73df4b3b58864d0d35836f1dc2c
manhvipro123 <manhvp1810@gmail.com>
2022-03-10 09:57:18 +0700

    Update project 10/3/2022

--- SFYL/src/app/components/dialog-login/dialog-login.component.ts
+++ SFYL/src/app/components/dialog-login/dialog-login.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { LoginGGService } from 'src/app/services/login-gg.service';
 import { MatDialog } from '@angular/material/dialog';
 import { DialogRegisterComponent } from '../dialog-register/dialog-register.component';
+import { RecoverAccountComponent } from '../recover-account/recover-account.component';
 @Component({
   selector: 'app-dialog-login',
   templateUrl: './dialog-login.component.html',
@@ -11,9 +12,14 @@ export class DialogLoginComponent implements OnInit {
   value1 = '';
   value2 = '';
   constructor(private login: LoginGGService, public dialog: MatDialog) {}
-  openDialog() {
+  openDialog1() {
     this.dialog.open(DialogRegisterComponent);
   }
+  openDialog2() {
+    this.dialog.open(RecoverAccountComponent);
+  }
+
+
 
   ngOnInit(): void {}
   public Login() {
