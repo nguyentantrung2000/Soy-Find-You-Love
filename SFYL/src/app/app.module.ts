8f9b88b5b47d1741b2a71f728c0c82092c6e7773
nguyentantrung2000 <trung.nt4996@sinhvien.hoasen.edu.vn>
2022-03-11 10:59:48 +0700

    pull

--- SFYL/src/app/app.module.ts
+++ SFYL/src/app/app.module.ts
@@ -2,16 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
 import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
-import { environment } from '../environments/environment';
 import { provideAuth, getAuth } from '@angular/fire/auth';
 import { provideFirestore, getFirestore } from '@angular/fire/firestore';
+
+import { environment } from '../environments/environment';
+
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ShareModule } from './share/share/share.module';
-
-
-
 
 @NgModule({
   declarations: [AppComponent],
--- SFYL/src/app/pages/layout/setting/setting.component.ts
+++ SFYL/src/app/pages/layout/setting/setting.component.ts
@@ -1,15 +1,18 @@
 import { Component, OnInit } from '@angular/core';
-import {ThemePalette} from '@angular/material/core';
-import {ProgressBarMode} from '@angular/material/progress-bar';
+import { ThemePalette } from '@angular/material/core';
+import { ProgressBarMode } from '@angular/material/progress-bar';
 import { MatDialog } from '@angular/material/dialog';
 import { DialogLogoutComponent } from 'src/app/components/dialog-logout/dialog-logout.component';
 import { DialogDeleteAccComponent } from 'src/app/components/dialog-delete-acc/dialog-delete-acc.component';
 @Component({
   selector: 'app-setting',
   templateUrl: './setting.component.html',
-  styleUrls: ['./setting.component.scss']
+  styleUrls: ['./setting.component.scss'],
 })
 export class SettingComponent implements OnInit {
+  lat: any;
+  lng: any;
+  a: any;
   formatLabel(value: number) {
     if (value >= 1000) {
       return Math.round(value / 1000) + 'km';
@@ -19,12 +22,12 @@ export class SettingComponent implements OnInit {
   }
   formatAge(value: number) {
     if (value >= 1000) {
-      return Math.round(value / 1000) ;
+      return Math.round(value / 1000);
     }
 
     return value;
   }
-  constructor(public dialog: MatDialog) { }
+  constructor(public dialog: MatDialog) {}
 
   openDialog() {
     this.dialog.open(DialogLogoutComponent);
@@ -32,8 +35,31 @@ export class SettingComponent implements OnInit {
   openDialogDelete() {
     this.dialog.open(DialogDeleteAccComponent);
   }
-  ngOnInit(): void {
+  public Distance() {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(async (position) => {
+        this.lat = position.coords.latitude;
+        this.lng = position.coords.longitude;
+        var apikey = '8cdaef3a4f7041548629225833ddd204';
+
+        var api_url = 'https://api.opencagedata.com/geocode/v1/json';
+
+        var request_url =
+          api_url +
+          '?' +
+          'key=' +
+          apikey +
+          '&q=' +
+          encodeURIComponent(this.lat + ',' + this.lng) +
+          '&pretty=1' +
+          '&no_annotations=1';
+        // await this.http.get(request_url).subscribe(async (res: any) => {
+        //   this.a = await res;
+        //   console.log(res.results[0].formatted);
+        //   this.cal();
+        // });
+      });
+    }
   }
-
-
+  ngOnInit(): void {}
 }
