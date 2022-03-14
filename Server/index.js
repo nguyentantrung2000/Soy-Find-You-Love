adf96157d279befbff83e719db93283f8005fc5f
nguyentantrung2000 <trung.nt4996@sinhvien.hoasen.edu.vn>
2022-03-14 22:09:51 +0700

    like api update

--- Server/index.js
+++ Server/index.js
@@ -90,12 +90,24 @@ server.put("/user/update", async(request, response) => {
 server.post("/user/likelist", async(request, response) => {
 
         let collectionName = request.body.collectionName;
-        let docId = request.body.docId;
-        let docIDs = request.body.docIDs;
-        await firebase.firestore().collection(collectionName).doc(docId).update({
-            Like: firebase.firestore.FieldValue.arrayUnion(docIDs)
-        });
-
+        let docId = request.body.docId; ////nguoi dung
+        let docIDs = request.body.docIDs; ////nguoi dung duoc thich 
+        let isExits = await firebase.firestore().collection(collectionName).doc(docIDs).get();
+        console.log(isExits.data().Like);
+        for (let i = 0; i < isExits.data().Watting.length; i++) {
+            if (docId == i) {
+                await firebase.firestore().collection(collectionName).doc(docId).update({
+                    Like: firebase.firestore.FieldValue.arrayUnion(docIDs)
+                });
+            } else {
+                await firebase.firestore().collection(collectionName).doc(docId).update({
+                    Watting: firebase.firestore.FieldValue.arrayUnion(docIDs)
+                });
+            }
+        }
+        // await firebase.firestore().collection(collectionName).doc(docId).update({
+        //     Like: firebase.firestore.FieldValue.arrayUnion(docIDs)
+        // });
         response.send({
             message: "Like"
         })
