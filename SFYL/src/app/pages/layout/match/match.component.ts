f7c55f7ea14c96f61105fb885106deec69f62885
tranvanhao016a <hao.tv1665@sinhvien.hoasen.edu.vn>
2022-03-15 17:52:33 +0700

    Hao

--- SFYL/src/app/pages/layout/match/match.component.ts
+++ SFYL/src/app/pages/layout/match/match.component.ts
@@ -10,4 +10,36 @@ export class MatchComponent implements OnInit {
   constructor(public userData: DataService,public httpSv: HttpClientService) {}
 
   ngOnInit(): void {}
+   // public async addData(){
+  //   await (await this.httpSv.postData(this.name,this.pirce,this.photoURL,this.popularity,this.quantity,this.collectionName))
+  //   .subscribe((value: any) =>{
+  //       alert(value['message']);
+  //   });
+  // }
+  // public async Login() {
+  //   this.login.loginGG();
+
+  //   if (this.login.user != null) {
+  //     await this.http
+  //       .post(
+  //         environment.endpoint + 'user',
+  //         {
+  //           collectionName: 'User',
+  //           data: {
+  //             email: this.login.user?.email,
+  //             name: this.login.user?.displayName,
+  //             photoURL: this.login.user?.photoURL,
+  //             Location: [],
+  //             Like: [],
+  //             unLike: [],
+  //             Watting: [],
+  //           },
+  //         },
+  //         { responseType: 'text' }
+  //       )
+  //       .toPromise();
+  //     this.dialog.closeAll();
+  //   }
+  // }
+
 }
--- SFYL/src/app/services/http-client.service.ts
+++ SFYL/src/app/services/http-client.service.ts
@@ -2,28 +2,61 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http'; 
 import { environment } from 'src/environments/environment';
 import { Observable } from 'rxjs';
+
 @Injectable({
   providedIn: 'root'
 })
 export class HttpClientService {
 
   constructor(public http: HttpClient){}
+
+  //get Userall
   public async getUser(apiPath:string):Promise<Observable<any>> {
    let result = this.http.get(environment.endpoint+apiPath);
    return result;
   }
-  
 
-  public async getLike(apiPath:string):Promise<Observable<any>> {
-   let result = this.http.get(environment.endpoint+apiPath);
-   return result;
+
+  ///get UserId
+  public getUserId(params:string){
+    return this.http.get(environment.endpoint+`/user/${params}`);
   }
-  public async getDisLike(apiPath:string):Promise<Observable<any>> {
-    let result = this.http.get(environment.endpoint+apiPath);
-    return result;
-   }
 
-   
+  /// Post Like
+  public async LikeService(collectionName:string,docId:string,docIDs:string){
+    return this.http.post(environment.endpoint+'/user/likelist',{
+      data:{
+        collectionName: collectionName,
+        docId:docId,
+        docIDs:docIDs
+      }
+    });
+  }
+  public async UnLikeService(collectionName:string,docId:string,docIDs:string){
+    return this.http.post(environment.endpoint+'/user/unlikelist',{
+      data:{
+        collectionName: collectionName,
+        docId:docId,
+        docIDs:docIDs
+      }
+
+    });
+
+  }
+
+   public async UpdateData(collectionName:string,docId:string,gioitinh:string,noio:string,ngaysinh:string,sothich:[]){
+     return this.http.put(environment.endpoint+'/user/update',{
+       data:{
+         gioitinh : gioitinh,
+         noio : noio,
+         ngaysinh : ngaysinh,
+         sothich :  sothich       
+       },
+       collectionName : collectionName,
+       docId : docId,
+
+     })
+   }
   // public async postData(name:string,pirce:number,photoURL:string,popularity:string,quantity:number,collectionName:string){
   //  return this.http.post(environment.endpoint+'api',{
   //     data:{name:name,
