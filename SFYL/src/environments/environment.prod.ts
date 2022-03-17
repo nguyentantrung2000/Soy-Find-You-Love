388a362b19ae20068e26fa6261777d1e8a308c59
nguyentantrung2000 <trung.nt4996@sinhvien.hoasen.edu.vn>
2022-03-18 02:48:09 +0700

    push

--- SFYL/src/environments/environment.prod.ts
+++ SFYL/src/environments/environment.prod.ts
@@ -3,9 +3,11 @@ export const environment = {
     projectId: 'sfyl-d556e',
     appId: '1:1044351428939:web:664d13ef5f33179fd1a0ce',
     storageBucket: 'sfyl-d556e.appspot.com',
+    locationId: 'asia-northeast3',
     apiKey: 'AIzaSyAoLEx2-QkIUd_QdJLb33jt-cFfsYPKtog',
     authDomain: 'sfyl-d556e.firebaseapp.com',
     messagingSenderId: '1044351428939',
   },
-  production: true
+  production: true,
+  endpoint: 'https://soyfindyourlovee.herokuapp.com/',
 };
--- SFYL/src/environments/environment.ts
+++ SFYL/src/environments/environment.ts
@@ -7,6 +7,7 @@ export const environment = {
     projectId: 'sfyl-d556e',
     appId: '1:1044351428939:web:664d13ef5f33179fd1a0ce',
     storageBucket: 'sfyl-d556e.appspot.com',
+    locationId: 'asia-northeast3',
     apiKey: 'AIzaSyAoLEx2-QkIUd_QdJLb33jt-cFfsYPKtog',
     authDomain: 'sfyl-d556e.firebaseapp.com',
     messagingSenderId: '1044351428939',
